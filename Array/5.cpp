cadb894d343efa9cd37b21172287756319ee060b
MD Aawesh <50728773+aawesh2000@users.noreply.github.com>
2021-06-20 21:17:21 +0530

    Update 5.cpp

--- Array/5.cpp
+++ Array/5.cpp
@@ -1,7 +1,7 @@
 //Move all negative numbers to beginning and positive to end with constant extra space
 #include<bits/stdc++.h>
 using namespace std;
- 
+//without sorting, JUST SEGRIGATING +ve and -ve numbers
 
 int main(){
     int n, i, arr[1000], k;
@@ -27,20 +27,22 @@ int main(){
             int temp=arr[left];
             arr[left]=arr[right];
             arr[right]=temp;
-            left+=1;
-            right-=1;
+            left++;
+            right--;
         }
-        else if (arr[left]>0 && arr[right] >0)
-            right-=1;
-            else{
-                left += 1;
-                right -= 1;
+        else if(arr[left]>0 && arr[right] >0){
+            right--;
+        }
+        else{
+            left++;
+            right--;
             }
     }
         
     for(i=0; i<n; i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
   
 }
 
